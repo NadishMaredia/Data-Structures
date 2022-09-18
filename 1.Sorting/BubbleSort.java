4a6861c2b03007652eebc6374d65fc62f7ea9baf
Nadish Maredia <nadishns97@gmail.com>
2022-09-18 16:38:02 -0400

    Added: Bubble Sort

---
+++ 1.Sorting/BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort {
+    public void sort(int[] arr) {
+        int n = arr.length;
+
+        for(int i = 0;i<n-1;i++) {
+            for(int j = 0;j<n-i-1;j++) {
+                if(arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
