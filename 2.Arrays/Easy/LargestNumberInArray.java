c35b1b929426995d7dfd1f45614eb56d318c9f52
Nadish Maredia <nadishns97@gmail.com>
2022-09-18 17:18:54 -0400

    Added: Largest Number in Array

---
+++ 2.Arrays/Easy/LargestNumberInArray.java
@@ -0,0 +1,13 @@
+public class LargestNumberInArray {
+    
+    public int largest(int[] arr, int n) {
+        int max = Integer.MIN_VALUE;
+        for(int i = 0;i<n;i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+}
