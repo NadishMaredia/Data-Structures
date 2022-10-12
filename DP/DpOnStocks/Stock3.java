2a757c0723616a466fe21c4e694ace86a45c7ad4
Nadish Maredia <nadishns97@gmail.com>
2022-10-12 09:03:31 -0400

    Added: Best time to buy and sell stock 3:

---
+++ DP/DpOnStocks/Stock3.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/description/
+package DP.DpOnStocks;
+
+public class Stock3 {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n][2][3];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<2; j++){
+                for(int k=0; k<3; k++){
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+        return solve(0, 0, 2, prices, dp);
+    }
+
+    public int solve(int index, int buy, int limit, int[] prices, int[][][] dp) {
+
+        if(index == prices.length || limit == 0) return 0;
+        
+        if(dp[index][buy][limit] != -1) return dp[index][buy][limit];
+
+        int profit = 0;
+        if(buy == 0) {
+            // have to buy
+            int pick = -prices[index] + solve(index+1, 1, limit, prices, dp);
+            int notPick = 0 + solve(index+1, 0, limit, prices, dp);
+            profit = Math.max(pick, notPick);
+        } else {
+            // have to sell
+            int pick = prices[index] + solve(index+1, 0, limit - 1, prices, dp);
+            int notPick = 0 + solve(index+1, 1, limit, prices, dp);
+            profit = Math.max(pick, notPick);
+        }
+
+        return dp[index][buy][limit] = profit;
+    }
+}
